8e09034b9fdd0159e4fd72229ff7c4a6e7910f42
Kyle Maggard <kjm0001@gmail.com>
2017-03-08 17:50:09 +0000

    Many to Many Association User and Stock Sec 8 Lect 170 fixed user_stocks create action

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -24,12 +24,12 @@ class UserStocksController < ApplicationController
   # POST /user_stocks
   # POST /user_stocks.json
   def create
-    if params[:stock_id].present ?
+    if params[:stock_id].present?
       @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)  
     else
       stock = Stock.find_by_ticker(params[:stock_ticker])
       if stock 
-        @user_stock = UserStock.new(user: current_user. stock: stock)
+        @user_stock = UserStock.new(user: current_user, stock: stock)
       else
         stock = Stock.new_from_lookup(params[:stock_ticker])
         if stock.save
@@ -38,7 +38,9 @@ class UserStocksController < ApplicationController
           @user_stock = nil
           flash[:error] = "Stock is not available"
         end  
+      
       end  
+    
     end  
     
 
